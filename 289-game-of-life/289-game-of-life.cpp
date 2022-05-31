551c74c58804bc6d1dd53a0c26237ed5de980d2a
Abhishek Jangid <81578100+abhishekvijay20@users.noreply.github.com>
2022-05-31 12:30:43 +0530

    Time: 6 ms (15.85%), Space: 7 MB (20.68%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -7,27 +7,29 @@ public:
             for (int j=c-1; j<=c+1; j++) {
                 if (j < 0 || j >= n) continue;
                 if (i == r && j == c) continue;
-                if (board[i][j] == 1) cnt1++;
+                if (board[i][j] == 1 || board[i][j] == 3) cnt1++;
             }
         }
     }
     
     void gameOfLife(vector<vector<int>>& board) {
-        int m = board.size(), n = board[0].size();
-        vector<vector<int>> aux(m, vector<int>(n, 0));
-                
+        int m = board.size(), n = board[0].size();             
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
                 int cnt1 = 0;
                 neighbors(board, i, j, cnt1);
                 if (board[i][j] == 0) {
-                    if (cnt1 == 3) aux[i][j] = 1;
+                    if (cnt1 == 3) board[i][j] = 2;
                 } else {
-                   if (cnt1 ==2 || cnt1 ==3) aux[i][j] = 1;
+                   if (!(cnt1 ==2 || cnt1 ==3)) board[i][j] = 3;
                 }
             }
         }
-        
-        for (int i=0; i<m; i++) for (int j=0; j<n; j++) board[i][j] = aux[i][j];
+        for (int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(board[i][j] == 2) board[i][j] = 1;
+                else if(board[i][j] == 3) board[i][j] = 0;
+            }
+        }
     }
 };
