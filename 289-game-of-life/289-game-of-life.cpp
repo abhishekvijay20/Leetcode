82a041002be5cfc866ef12f3cc7b58da466f2101
Abhishek Jangid <81578100+abhishekvijay20@users.noreply.github.com>
2022-05-31 11:42:10 +0530

    Time: 4 ms (26.66%), Space: 7.1 MB (20.68%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void neighbors(vector<vector<int>>& board, int r, int c, int &cnt0, int& cnt1) {
+        int m = board.size(), n = board[0].size();
+        for (int i=r-1; i<=r+1; i++) {
+            if (i < 0 || i >= m) continue;
+            for (int j=c-1; j<=c+1; j++) {
+                if (j < 0 || j >= n) continue;
+                if (i == r && j == c) continue;
+                if (board[i][j] == 0) cnt0++;
+                else cnt1++;
+            }
+        }
+    }
+    
+    void gameOfLife(vector<vector<int>>& board) {
+        int m = board.size(), n = board[0].size();
+        vector<vector<int>> aux(m, vector<int>(n, 0));
+                
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                int cnt0 = 0, cnt1 = 0;
+                neighbors(board, i, j, cnt0, cnt1);
+                if (board[i][j] == 0) {
+                    if (cnt1 == 3) aux[i][j] = 1;
+                } else {
+                   if (cnt1 ==2 || cnt1 ==3) aux[i][j] = 1;
+                }
+            }
+        }
+        
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                board[i][j] = aux[i][j];
+                // cout << aux[i][j] << " ";
+            }
+        }
+    }
+};
