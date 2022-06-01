8d82e59b66fe6564bfd94e9c2c57e5d761f80274
Abhishek Jangid <81578100+abhishekvijay20@users.noreply.github.com>
2022-06-01 17:42:56 +0530

    Time: 8 ms (99.06%), Space: 16.7 MB (52.34%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -2,27 +2,14 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        if (n == 1) return 0;
-        if (n == 2) return 1;
-        
-        vector<int> aux(n, 0);
-        
-        if (nums[n-2] != 0) aux[n-2] = 1;
-        else aux[n-2] = 10000;
-        
-        for (int i=n-3; i>=0; i--) {
-            int mini = 10000;
-            for (int j=0; j < min(nums[i], n-i-1); j++) {
-                if (aux[i+j+1] < mini) {
-                    mini = aux[i+j+1];
-                }
+        int begin = 0, end = 0, farthest = 0, jump = 0;
+        for (int i=0; i<n-1; i++) {
+            farthest = max(farthest, i+nums[i]);
+            if (i == end) {
+                jump++;
+                end = farthest;
             }
-            aux[i] = mini+1;
         }
-        
-        // for (int i=0; i<n; i++) {
-        //     cout << aux[i] << " ";
-        // }
-        return aux[0];
+        return jump;
     }
 };
