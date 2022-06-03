32375f6ca85ba5c98955c49592d8374bba6d0018
Abhishek Jangid <81578100+abhishekvijay20@users.noreply.github.com>
2022-06-04 01:39:29 +0530

    Time: 84 ms (62.41%), Space: 48.3 MB (77.90%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,49 +2,11 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<bool> canReach(n, false);
-        canReach[n-1] = true;
-            
-        for (int i=n-2; i>=0; i--) {
-            
-            for (int j=1; j<=nums[i]; j++) {
-                
-                if (i+j < n && canReach[i+j] == true) {
-                    canReach[i] = true;
-                    break;
-                }
-            }
+        int i = 0, reach = 0;
+        while (i < n && i <= reach) {
+            reach = max (reach, i+nums[i]);
+            i++;
         }
-
-        return canReach[0];
+        return i == n;
     }
 };
-
-
-        // int n = nums.size();
-        // vector<bool> canReach(n);
-        // canReach[n-1] = true;
-        // for (int i=0; i<n; i++) {
-        //     cout << canReach[i] << " ";
-        // }      
-        // cout << endl;
-        // for (int i=n-2; i >= 0; i--) {
-        //     if (nums[i] >= 1) {
-        //         for (int j=1; j<=nums[i]; j++) {
-        //             if (i+j < n && nums[i+j] == true) {
-        //                 cout << "aaya" << endl;
-        //                 nums[i] = true;
-        //                 break;
-        //             } else {
-        //                 canReach[i] = false;
-        //             }
-        //         }
-        //     } else {
-        //         cout << "aaya" << endl;
-        //         canReach[i] = false;
-        //     }
-        // }
-        // for (int i=0; i<n; i++) {
-        //     cout << canReach[i] << " ";
-        // }
-        // return canReach[0];
