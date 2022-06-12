6ee6ea732be804eb3c78becca2ad2ea257d6d2cc
Abhishek Jangid <81578100+abhishekvijay20@users.noreply.github.com>
2022-06-13 01:35:29 +0530

    Time: 3 ms (97.31%), Space: 8.4 MB (95.10%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,16 +1,22 @@
 class Solution {
 private:
-    void helper (vector<vector<int>>& ans, vector<int> nums, int ind) {
-        if (ind >= nums.size()) {
+    bool checkmiddle(vector<int>& nums, int i , int begin){
+        for(int k = begin; k<i; k++)
+            if(nums[i] == nums[k])
+                return true;
+        return false;
+    }
+    void helper (vector<vector<int>>& ans, vector<int>& nums, int ind) {
+        if (ind >= nums.size()-1) {
             ans.push_back(nums);
             return;
         }
         
         for (int i=ind; i<nums.size(); i++) {
-            if (i != ind && nums[i] == nums[ind]) continue;
+            if ((i != ind && nums[i] == nums[ind]) || checkmiddle(nums, i, ind)) continue;
             swap(nums[i], nums[ind]);
             helper(ans, nums, ind+1);
-            //swap(nums[i], nums[ind]);
+            swap(nums[i], nums[ind]);
         }
     }
 public:
