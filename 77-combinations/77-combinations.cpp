3b4438b694075c435e65c08eec5d2f75bae0c620
Abhishek Jangid <81578100+abhishekvijay20@users.noreply.github.com>
2022-06-12 12:08:32 +0530

    Time: 899 ms (5.28%), Space: 164.3 MB (12.90%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,37 +1,22 @@
 class Solution {
 private:
-    vector<vector<int>> helper(vector<int>& nums, int k, int ind) {
-        if (k == 1) {
-            vector<vector<int>> v;
-            for (int i=ind; i<nums.size(); i++) {
-                v.push_back({nums[i]});
-            }
-            return v;
+    void helper(vector<vector<int>>& ans, vector<int> v, int k, int n, int ind) {
+        if (v.size() == k) {
+            ans.push_back(v);
+            return;
         }
         
-        if (ind + k -1 >= nums.size()) {
-            return {};
+        for (int i=ind; i<n; i++) {
+            v.push_back(i+1);
+            helper(ans, v, k, n, i+1);
+            v.pop_back();
         }
-
-        vector<vector<int>> smallAns1 = helper(nums, k, ind+1);
-        vector<vector<int>> smallAns2 = helper(nums, k-1, ind+1);
-        
-        vector<vector<int>> ans;
-        for (int i=0; i<smallAns1.size(); i++) {
-            ans.push_back(smallAns1[i]);
-        }
-        for (int i=0; i<smallAns2.size(); i++) {
-            smallAns2[i].push_back(nums[ind]);
-            ans.push_back(smallAns2[i]);
-        }
-        return ans;
     }
 public:
     vector<vector<int>> combine(int n, int k) {
-        vector<int> nums;
-        for (int i=1; i<=n; i++) {
-            nums.push_back(i);
-        }        
-        return helper(nums, k, 0);
+        vector<vector<int>> ans;
+        vector<int> v;
+        helper(ans, v, k, n, 0);
+        return ans;
     }
 };
