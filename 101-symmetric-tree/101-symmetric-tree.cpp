744685aebb764478e68f10afc0dace262888a541
Abhishek Jangid <81578100+abhishekvijay20@users.noreply.github.com>
2022-07-27 00:11:02 +0530

    Time: 0 ms (100.00%), Space: 16.4 MB (47.93%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -12,9 +12,8 @@
 class Solution {
 private:
     bool helper(TreeNode* root1, TreeNode* root2) {
-        if (root1 == NULL && root2 == NULL) return true;
-        if (root1 == NULL && root2 != NULL) return false;
-        if (root1 != NULL && root2 == NULL) return false;
+        if (root1 == NULL || root2 == NULL) return root1 == root2;
+        
         if (root1->val != root2->val) return false;
         
         return helper(root1->left, root2->right) && helper(root1->right, root2->left);
