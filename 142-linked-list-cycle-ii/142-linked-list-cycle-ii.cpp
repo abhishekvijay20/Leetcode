2567120deed3b98d9e974841aadf4678df52edb2
Abhishek Jangid <81578100+abhishekvijay20@users.noreply.github.com>
2022-09-12 16:01:23 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (75.18%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -8,16 +8,24 @@
  */
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*> address;
-        ListNode* temp = head;
-        while (temp) {
-            if (address.find(temp) != address.end()) {
-                return temp;
-            }
-            address.insert(temp);
-            temp = temp->next;
+    ListNode* collidePoint(ListNode* head) {
+        ListNode* slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return slow;
         }
-        return NULL;
+        return NULL;        
+    }
+    
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* point = collidePoint(head), *entry = head;
+        if (point == NULL) return NULL;
+        
+        while (entry != point) {
+            entry = entry->next;
+            point = point->next;
+        }
+        return point;
     }
 };
